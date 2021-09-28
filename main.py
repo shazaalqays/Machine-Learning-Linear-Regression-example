2afb25a2b00469f93644b21e5bb85afbfbaa7192
shazaalqays <37289506+shazaalqays@users.noreply.github.com>
2021-09-28 16:12:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import pandas
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+
+data = pandas.read_csv('cost_revenue_clean.csv')
+
+print(data.describe())
+
+X = DataFrame(data, columns=['production_budget_usd'])
+y = DataFrame(data, columns=['worldwide_gross_usd'])
+
+plt.figure(figsize=(10,6))
+plt.scatter(X, y, alpha=0.3)
+plt.title('Film Cost vs Global Revenue')
+plt.xlabel('Production Budget $')
+plt.ylabel('Worldwide Gross $')
+plt.ylim(0, 3000000000)
+plt.xlim(0, 450000000)
+plt.show()
+
+regression = LinearRegression()
+regression.fit(X, y)
+
+regression.coef_    # theta_1
+
+regression.intercept_
+
+plt.figure(figsize=(10,6))
+plt.scatter(X, y, alpha=0.3)
+
+# Adding the regression line here:
+plt.plot(X, regression.predict(X), color='red', linewidth=3)
+
+plt.title('Film Cost vs Global Revenue')
+plt.xlabel('Production Budget $')
+plt.ylabel('Worldwide Gross $')
+plt.ylim(0, 3000000000)
+plt.xlim(0, 450000000)
+plt.show()
+
+print(regression.score(X, y))
